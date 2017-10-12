b1d7167bbddc0fb33c98fb231fdaa09565cc08f8
Vedran Mijatović <vedranmijatovic0@gmail.com>
2017-10-12 11:24:42 +0200

    change repo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# User.create(email: "vedran.mijatovic@bamboolab.eu", password: "adminadmin", admin: true)
+User.create(email: "vedran.mijatovic@bamboolab.eu", password: "adminadmin", admin: true)
 User.create(email: "contact@bamboolab.eu", password: "adminadmin", admin: true)
-# puts "Users created"
-# 26.times do
-#   Profession.create(name: Faker::Company.unique.profession)
-# end
-# puts "Professions created"
-# 150.times do
-#   Person.create(name: Faker::Name.name, email: Faker::Internet.email, dob: Faker::Date.between(60.years.ago, 15.years.ago), profession_id: rand(25)+1)
-# end
-# puts "People created"
-# 5.times do
-#   Dessert.create(name: Faker::Dessert.unique.variety)
-# end
-# puts "Desserts created"
-# Person.all.each do |user|
-#   (1..5).to_a.sample(rand(3)).each do |id|
-#     user.favorite_desserts.create(dessert_id: id)
-#   end
-# end
-# puts "Favorite desserts created"
+puts "Users created"
+26.times do
+  Profession.create(name: Faker::Company.unique.profession)
+end
+puts "Professions created"
+150.times do
+  Person.create(name: Faker::Name.name, email: Faker::Internet.email, dob: Faker::Date.between(60.years.ago, 15.years.ago), profession_id: rand(25)+1)
+end
+puts "People created"
+5.times do
+  Dessert.create(name: Faker::Dessert.unique.variety)
+end
+puts "Desserts created"
+Person.all.each do |user|
+  (1..5).to_a.sample(rand(3)).each do |id|
+    user.favorite_desserts.create(dessert_id: id)
+  end
+end
+puts "Favorite desserts created"
